2fc49dc7c91435bc2faa74c0c04e2e853d26a9c5
Weijia Chen <chenweijiauk@hotmail.com>
2014-08-28 23:03:23 +0100

    Update Quiz3.R

--- Quiz3.R
+++ Quiz3.R
@@ -8,3 +8,14 @@ agricultureLogical <- q3.1$ACR == 3 & q3.1$AGS == 6
 row3.1 <- q3.1[which(agricultureLogical), c(11,12)]
 head(row3.1, 3)
 
+#Q2
+#Download the pic and change the file name to q3.2
+#install jpeg package
+#load jpeg package
+library(jpeg)
+#read file
+q3.2 <- readJPEG("./GCD/q3.2.jpg", native = TRUE)
+#calculate 30% and 80% quantile
+quantile(q3.2, probs = c(0.3, 0.8))
+
+#Q3
