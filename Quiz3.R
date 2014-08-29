9e83e766641312beca071c6dd8c6756dad2e79be
Weijia Chen <chenweijiauk@hotmail.com>
2014-08-29 06:22:28 +0100

    Update Quiz3.R

--- Quiz3.R
+++ Quiz3.R
@@ -19,3 +19,18 @@ q3.2 <- readJPEG("./GCD/q3.2.jpg", native = TRUE)
 quantile(q3.2, probs = c(0.3, 0.8))
 
 #Q3
+#change GDP file name to q3.3GDP, change EDU file name to q3.3Edu
+#load two files
+gdp <- read.csv("./GCD/q3.3GDP.csv")
+#cleaning gdp data
+#remove row 1, 2 and 4
+gdp <- gdp[c(-1,-2,-4),]
+#rename variable name
+names(gdp)[names(gdp) == "X"] <- "CC" #CC means CountryCode
+names(gdp)[names(gdp) == "Gross.domestic.product.2012"] <- "Ranking"
+names(gdp)[names(gdp) == "X.2"] <- "Economy"
+names(gdp)[names(gdp) == "X.3"] <- "UD"
+#remove row 1
+gdp <- gdp[-1,]
+#subset gdp, new gdp include 190 countries and 4 variables
+gdp <- gdp[, c(1,2,4,5)]
